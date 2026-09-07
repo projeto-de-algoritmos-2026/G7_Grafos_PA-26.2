f34bf0476be6b9e23ff402b096c28dd98f41c84a
juliagabriellafs <juliagabriellaf0407@gmail.com>
2026-09-07 08:42:01 -0300

    feat: dijkstra inicializa a estrutura com infinito

--- backend/app/algoritmos/dijkstra.py
+++ backend/app/algoritmos/dijkstra.py
@@ -1,85 +1,25 @@
-class MinHeap:
-    def __init__(self):
-        self.heap = []
-        self.pos_map = {}
+import math
+from typing import Dict, List, Optional
+from .bellman_ford import RouteResult
+from .min_heap import MinHeap
 
-    def is_empty(self):
-        return len(self.heap) == 0
+def run_dijkstra(graph_nodes: Dict[int, dict], graph_edges: Dict[int, List[dict]], start_node: int, end_node: int) -> Optional[RouteResult]:
+    vertices = set([int(k) for k in graph_nodes.keys()])
+    
+    if start_node not in vertices:
+        return None
 
-    def parent(self, i):
-        return (i - 1) // 2
+    distances: Dict[int, float] = {}
+    predecessors: Dict[int, Optional[int]] = {}
+    visited_edges: List[Dict[str, int]] = []
+    
+    pq = MinHeap()
 
-    def left_child(self, i):
-        return 2 * i + 1
+    for v in vertices:
+        distances[v] = math.inf
+        predecessors[v] = None
 
-    def right_child(self, i):
-        return 2 * i + 2
+    distances[start_node] = 0.0
+    pq.insert(start_node, 0.0)
 
-    def swap(self, i, j):
-        node_i = self.heap[i]
-        node_j = self.heap[j]
-
-        self.pos_map[node_i['node']] = j
-        self.pos_map[node_j['node']] = i
-
-        self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
-
-def shift_up(self, i):
-        while i > 0:
-            parent_idx = self.parent(i)
-            
-            if self.heap[i]['dist'] < self.heap[parent_idx]['dist']:
-                self.swap(i, parent_idx)
-                i = parent_idx
-            else:
-                break
-def heapify(self, i):
-        size = len(self.heap)
-        
-        while True:
-            smallest = i
-            left = self.left_child(i)
-            right = self.right_child(i)
-
-            if left < size and self.heap[left]['dist'] < self.heap[smallest]['dist']:
-                smallest = left
-
-            if right < size and self.heap[right]['dist'] < self.heap[smallest]['dist']:
-                smallest = right
-
-            if smallest != i:
-                self.swap(i, smallest)
-                i = smallest
-            else:
-                break
-
-def insert(self, node_id, dist):
-        node = {'node': node_id, 'dist': dist}
-        self.heap.append(node)
-        
-        current_idx = len(self.heap) - 1
-        self.pos_map[node_id] = current_idx
-        
-        self.shift_up(current_idx)
-
-def extract_min(self):
-        if self.is_empty():
-            return None
-            
-        min_node = self.heap[0]
-        last_node = self.heap.pop()
-        del self.pos_map[min_node['node']]
-        
-        if not self.is_empty():
-            self.heap[0] = last_node
-            self.pos_map[last_node['node']] = 0
-            self.heapify(0)
-            
-        return min_node
-
-def decrease_key(self, node_id, new_dist):
-        if node_id in self.pos_map:
-            idx = self.pos_map[node_id]
-            if new_dist < self.heap[idx]['dist']:
-                self.heap[idx]['dist'] = new_dist
-                self.shift_up(idx)
+    return None
